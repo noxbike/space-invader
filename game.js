c78ba727a7092f543d6c67ecf1f74006632445e2
mickael morel <noxbike@gmail.com>
2020-05-27 11:13:24 +0000

    deplacement du vaisseau ajouter

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
 $(function(){
-    
+    $(document).keydown(function(e){
+        if(e.which == 39){
+            var VaX = parseInt($('#vaisseau-allie').css('left'));
+            if(VaX < 870){
+                $('#vaisseau-allie').css('left', VaX+30);
+            } 
+        }
+
+        if(e.which == 37){
+            var VaX = parseInt($('#vaisseau-allie').css('left'));
+            if(VaX > 10){
+                $('#vaisseau-allie').css('left', VaX-30);
+            }
+        }
+    });
 })
