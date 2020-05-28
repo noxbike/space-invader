51e5b338653f2b3b121938de4bf47fe1811046f0
mickael morel <noxbike@gmail.com>
2020-05-28 16:12:55 +0000

    missile ajouté avec tir

--- game.js
+++ game.js
@@ -1,17 +1,66 @@
 $(function(){
-    $(document).keydown(function(e){
-        if(e.which == 39){
-            var VaX = parseInt($('#vaisseau-allie').css('left'));
-            if(VaX < 870){
-                $('#vaisseau-allie').css('left', VaX+30);
-            } 
-        }
 
-        if(e.which == 37){
-            var VaX = parseInt($('#vaisseau-allie').css('left'));
-            if(VaX > 10){
-                $('#vaisseau-allie').css('left', VaX-30);
+    var hero = {
+        top:700,
+        left: 1000
+    };
+
+    var missiles = [];
+
+    $(document).keydown(function(e){
+       
+        //deplacement vers la droite
+        if(e.which === 39){
+            console.log('left');
+            if(hero.left <= 1380){
+                hero.left = hero.left + 10;
+                moveHero(); 
             }
         }
-    });
+
+        //deplacement vers la gauche
+        if(e.which === 37){
+            if(hero.left >= 500 ){
+                hero.left = hero.left - 10;
+                moveHero(); 
+            }
+        };
+       
+        //tir de missile
+        if(e.which == 32){
+            console.log('fire');
+            missiles.push({
+                left: hero.left - 450,
+                top: hero.top
+            })
+            drawMissiles();
+        }
+        
+    })
+
+    function moveHero() {
+          $('#hero').css('left', hero.left);
+    }
+
+    function moveMissile(){
+        for( var missile = 0; missile < missiles.length; missile++){
+            missiles[missile].top = missiles[missile].top - 5;
+        }
+    }
+
+    function drawMissiles(){
+        $('#missile').html('');
+        for( var missile = 0; missile < missiles.length; missile++){
+            $('<div/>', {class: 'missile', style: `top:${missiles[missile].top}px; left:${missiles[missile].left}px;` }).appendTo('#missile');
+        }
+        
+    }
+
+    function gameLoop(){
+        setTimeout(gameLoop , 10)
+        moveMissile();
+        drawMissiles();
+    }
+    gameLoop();
+
 })
